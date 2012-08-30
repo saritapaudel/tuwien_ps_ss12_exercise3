2a71c9e55ae625e8715adb28c95f97df374b9dbd
Michael Schröder <mc.schroeder@gmail.com>
2012-08-30 10:45:41 +0200

    data structure

--- db.hs
+++ db.hs
@@ -1,42 +1,25 @@
-import Data.List
-import Data.Time
+import Data.Map (Map)
+import qualified Data.Map as Map
 
-
-
-type DB = [Course]
-data Course = C CourseName [Registration] deriving (Show)
+type Courses = Map CourseName Registrations
+type Registrations = Map RegName RegData
+data RegData = RegData { timespan :: Timespan
+                       , constraints :: [Constraint]
+                       , students :: [Student]
+                       } deriving (Show)
 type CourseName = String
-data Registration = R RegName Timespan [Constraint] [Student] deriving (Show)
 type RegName = String
-type Timespan = (Int,Int) --(UTCTime,UTCTime)
-data Constraint = RequiredAny [RegName] | Disallowed RegName deriving (Show)
+type Timespan = (Int,Int) -- (UTCTime,UTCTime)
+data Constraint = RequireOneOf [RegName] | Forbid RegName deriving (Show)
 type Student = String
 
-testDB :: DB
-testDB = [C "FFP" [R "Kursanmeldung" (0,0) [] []]]
+db :: Courses
+db = Map.fromList [("FFP", Map.fromList [("Kursanmeldung", RegData (0,0) [] ["hans"])])]
 
+addCourse :: CourseName -> Courses -> Courses
+addCourse cname = Map.insert cname Map.empty
 
-addRegistration :: RegName -> Timespan -> CourseName -> DB -> DB
+addRegistration :: RegName -> Timespan -> CourseName -> Courses -> Courses
 addRegistration rname tspan cname cs = 
-    case find (\(C name _) -> name == cname) cs of
-        Nothing -> cs
-        Just (C name regs) -> cs'
-            where cs' = updateCourse c' cs
-                  c'  = C name (reg:regs)
-                  reg = R rname tspan [] []
+    Map.adjust (Map.insert rname (RegData tspan [] [])) cname cs
 
-removeRegistration :: RegName -> CourseName -> DB -> DB
-removeRegistration rname cname cs =
-    case find (\(C name _) -> name == cname) cs of
-        Nothing -> cs
-        Just (C name regs) -> cs'
-            where cs'   = updateCourse c' cs
-                  c'    = C name regs'
-                  regs' = removeReg rname regs
-                  removeReg n1 rs
-
-updateCourse :: Course -> [Course] -> [Course]
-updateCourse _ [] = []
-updateCourse (C name' regs') ((C name regs):cs)
-    | name' == name = (C name regs') : cs
-    | otherwise     = (C name regs) : updateCourse (C name' regs') cs
