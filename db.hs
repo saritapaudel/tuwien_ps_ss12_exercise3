50e25ae38fe84f3c92e925987a3db4bc6fdcb820
Michael Schröder <mc.schroeder@gmail.com>
2012-08-30 13:18:42 +0200

    basic constraints

--- db.hs
+++ db.hs
@@ -1,6 +1,8 @@
 import Data.Map (Map)
 import Data.List
 import qualified Data.Map as Map
+import Data.Maybe
+import Data.Either
 
 type Courses = Map CourseName Registrations
 type Registrations = Map RegName RegData
@@ -24,8 +26,30 @@ addRegistration :: RegName -> Timespan -> CourseName -> Courses -> Courses
 addRegistration rname tspan cname cs = 
     Map.adjust (Map.insert rname (RegData tspan [] [])) cname cs
 
+addConstraint :: Constraint -> RegName -> CourseName -> Courses -> Courses
+addConstraint con rname cname =
+    Map.adjust (Map.adjust addCon rname) cname
+    where addCon (RegData t cs ss) = RegData t (con:cs) ss
+
 allStudents :: Courses -> [Student]
 allStudents = nub . concat . outerFold
             where outerFold = Map.foldr innerFold []
                   innerFold = flip $ (Map.foldr concatStudents)
                   concatStudents = (:) . students
+
+isRegistered :: Student -> RegName -> CourseName -> Courses -> Bool
+isRegistered s rname cname cs = fromMaybe False $ do
+    regs <- Map.lookup cname cs
+    reg <- Map.lookup rname regs
+    return (s `elem` (students reg))
+
+--registerStudent :: Student -> RegName -> CourseName -> Courses -> ()
+
+violatedConstraints :: [Constraint] -> [RegName] -> [Constraint]
+violatedConstraints cs rs = filter (\c -> not $ testConstraint c rs) cs
+
+testConstraint :: Constraint -> [RegName] -> Bool
+testConstraint (RequireOneOf rs) = not . null . (intersect rs)
+testConstraint (Forbid r) = notElem r
+
+
