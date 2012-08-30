adbaab55ab30c24bac062f96347d3ad24a4d3447
Michael Schröder <mc.schroeder@gmail.com>
2012-08-30 20:33:30 +0200

    add regs, ugly

--- db.hs
+++ db.hs
@@ -2,7 +2,6 @@ import Data.Map (Map)
 import Data.List
 import qualified Data.Map as Map
 import Data.Maybe
-import Data.Either
 import Control.Monad.State
 import System.IO
 import System.Exit
@@ -29,9 +28,13 @@ addCourse = modify . flip Map.insert Map.empty
 removeCourse :: Monad m => CourseName -> StateT Courses m ()
 removeCourse = modify . Map.delete
 
-addRegistration :: RegName -> Timespan -> CourseName -> Courses -> Courses
-addRegistration rname tspan cname cs = 
-    Map.adjust (Map.insert rname (RegData tspan [] [])) cname cs
+addRegistration :: Monad m => RegName -> Timespan -> CourseName -> StateT Courses m ()
+addRegistration rname tspan cname = 
+    modify $ Map.adjust (Map.insert rname (RegData tspan [] [])) cname
+
+removeRegistration :: Monad m => RegName -> CourseName -> StateT Courses m ()
+removeRegistration rname cname = 
+    modify $ Map.adjust (Map.delete rname) cname
 
 addConstraint :: Constraint -> RegName -> CourseName -> Courses -> Courses
 addConstraint con rname cname =
@@ -57,10 +60,11 @@ testConstraint :: Constraint -> [RegName] -> Bool
 testConstraint (RequireOneOf rs) = not . null . intersect rs
 testConstraint (Forbid r) = notElem r
 
-
+main :: IO ()
 main = do
     let db = testdb -- TODO: load db
     runStateT (forever loop) db
+    return ()
 
 loop :: StateT Courses IO ()
 loop = do
@@ -75,14 +79,32 @@ loop = do
             liftIO $ mapM_ putStrLn students
         "addCourse" -> case maybeRead args of
             Nothing -> liftIO $ putStrLn "invalid input"
-            Just cname -> addCourse cname
+            Just (cname,_) -> addCourse cname
         "removeCourse" -> case maybeRead args of
             Nothing -> liftIO $ putStrLn "invalid input"
-            Just cname -> removeCourse cname
+            Just (cname,_) -> removeCourse cname
         "courses" -> do
             courses <- liftM Map.keys get
             liftIO $ mapM_ putStrLn courses
+        "addRegistration" -> case maybeRead args of
+            Nothing -> liftIO $ putStrLn "invalid input"
+            Just (rname,arg2) -> case maybeRead arg2 of
+                Nothing -> liftIO $ putStrLn "invalid input"                
+                Just (tspan,arg3) -> case maybeRead arg3 of
+                    Nothing -> liftIO $ putStrLn "invalid input"
+                    Just (cname,_) -> addRegistration rname tspan cname
+        "registrations" -> case maybeRead args of
+            Nothing -> liftIO $ putStrLn "invalid input"
+            Just (cname,_) -> do
+                courses <- get
+                let regs = Map.lookup cname courses
+                case regs of
+                    Nothing -> liftIO $ putStrLn "course not found"
+                    Just reg -> liftIO $ mapM_ putStrLn $ Map.keys reg
+        "debug" -> do
+            state <- get
+            liftIO $ putStrLn $ show state
         otherwise -> return ()
 
-maybeRead :: Read a => String -> Maybe a
-maybeRead = fmap fst . listToMaybe . reads
+maybeRead :: Read a => String -> Maybe (a,String)
+maybeRead = listToMaybe . reads
