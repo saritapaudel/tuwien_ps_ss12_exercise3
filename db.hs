212159582d1bf3f78e50ef392c6de96a8c01525d
Michael Schröder <mc.schroeder@gmail.com>
2012-08-31 16:19:31 +0200

    looking good

--- db.hs
+++ db.hs
@@ -6,6 +6,7 @@ import Control.Monad.State
 import System.IO
 import System.Exit
 import Data.Char
+import Control.Monad.Error
 
 type Courses = Map CourseName Registrations
 type Registrations = Map RegName RegData
@@ -87,123 +88,121 @@ registerStudent s rname cname cs =
 
 -- I/O
 
-main :: IO ()
-main = do
-    let db = testdb -- TODO: load db
-    runStateT (forever loop) db
-    return ()
+--loop :: StateT Courses IO ()
+--loop = do
+--    liftIO $ hSetBuffering stdin LineBuffering -- for GHCi
+--    liftIO $ putStr "> "
+--    liftIO $ hFlush stdout
+--    line <- liftIO getLine
+--    let (cmd,args) = break isSpace line
+--    case cmd of
+--        "courses" -> do
+--            cnames <- liftM courseNames get
+--            liftIO $ mapM_ putStrLn cnames
+--        "addCourse" -> do
+--            case parse (pString "course name") args of
+--                Left msg -> liftIO $ putStrLn msg
+--                Right (cname,_) -> modify (addCourse cname)
+--        "removeCourse" -> do
+--            courses <- get
+--            case parse (pCourse courses) args of
+--                Left msg -> liftIO $ putStrLn msg
+--                Right (cname,_) -> modify (removeCourse cname)
 
-loop :: StateT Courses IO ()
-loop = do
-    liftIO $ hSetBuffering stdin LineBuffering -- for GHCi
-    liftIO $ putStr "> "
-    liftIO $ hFlush stdout
-    line <- liftIO getLine
-    let (cmd,args) = break isSpace line
-    case cmd of
-        "courses" -> do
-            cnames <- liftM courseNames get
-            liftIO $ mapM_ putStrLn cnames
-        "addCourse" -> do
-            case parse (pString "course name") args of
-                Left msg -> liftIO $ putStrLn msg
-                Right (cname,_) -> modify (addCourse cname)
-        "removeCourse" -> do
-            courses <- get
-            case parse (pCourse courses) args of
-                Left msg -> liftIO $ putStrLn msg
-                Right (cname,_) -> modify (removeCourse cname)
+--        "regs" -> case maybeRead args of
+--            Nothing -> liftIO $ putStrLn "invalid input"
+--            Just (cname,_) -> do
+--                rnames <- liftM (regNames cname) get
+--                liftIO $ mapM_ putStrLn rnames
+--        "addReg" -> case parse_rname_tspan_cname args of
+--            Nothing -> liftIO $ putStrLn "invalid input"
+--            Just (rname,tspan,cname) -> modify (addReg rname tspan cname)
+--        "removeReg" -> case parse_rname_cname args of
+--            Nothing -> liftIO $ putStrLn "invalid input"
+--            Just (rname,cname) -> modify (removeReg rname cname)
 
-        "regs" -> case maybeRead args of
-            Nothing -> liftIO $ putStrLn "invalid input"
-            Just (cname,_) -> do
-                rnames <- liftM (regNames cname) get
-                liftIO $ mapM_ putStrLn rnames
-        "addReg" -> case parse_rname_tspan_cname args of
-            Nothing -> liftIO $ putStrLn "invalid input"
-            Just (rname,tspan,cname) -> modify (addReg rname tspan cname)
-        "removeReg" -> case parse_rname_cname args of
-            Nothing -> liftIO $ putStrLn "invalid input"
-            Just (rname,cname) -> modify (removeReg rname cname)
+--        "addCon" -> case parse_con_rname_cname args of
+--            Nothing -> liftIO $ putStrLn "invalid input"
+--            Just (con,rname,cname) -> modify (addConstraint con rname cname)
 
-        "addCon" -> case parse_con_rname_cname args of
-            Nothing -> liftIO $ putStrLn "invalid input"
-            Just (con,rname,cname) -> modify (addConstraint con rname cname)
+--        "students" -> do
+--            students <- liftM allStudents get
+--            liftIO $ mapM_ putStrLn students
+--        "studentRegs" -> case maybeRead args of
+--            Nothing -> liftIO $ putStrLn "invalid input"
+--            Just (student,_) -> do
+--                regs <- liftM (studentRegs student) get
+--                liftIO $ print regs
 
-        "students" -> do
-            students <- liftM allStudents get
-            liftIO $ mapM_ putStrLn students
-        "studentRegs" -> case maybeRead args of
-            Nothing -> liftIO $ putStrLn "invalid input"
-            Just (student,_) -> do
-                regs <- liftM (studentRegs student) get
-                liftIO $ print regs
-        "registerStudent" -> case parse_stud_rname_cname args of
-            Nothing -> liftIO $ putStrLn "invalid input"
-            Just (stud,rname,cname) -> do
-                db <- get
-                case registerStudent stud rname cname db of
-                    Left vcons -> do
-                        liftIO $ putStrLn "error: violates constraints:"
-                        liftIO $ mapM_ print vcons
-                    Right db' -> put db'
+--        "registerStudent" -> case parse_stud_rname_cname args of
+--            Nothing -> liftIO $ putStrLn "invalid input"
+--            Just (stud,rname,cname) -> do
+--                db <- get
+--                case registerStudent stud rname cname db of
+--                    Left vcons -> do
+--                        liftIO $ putStrLn "error: violates constraints:"
+--                        liftIO $ mapM_ print vcons
+--                    Right db' -> put db'
 
-        "debug" -> do
-            state <- get
-            liftIO $ print state
-        "quit" -> do
-            liftIO $ hSetBuffering stdin NoBuffering -- for GHCi
-            liftIO exitSuccess -- TODO: save db
-        otherwise -> return ()
+--        "debug" -> do
+--            state <- get
+--            liftIO $ print state
+--        "quit" -> do
+--            liftIO $ hSetBuffering stdin NoBuffering -- for GHCi
+--            liftIO exitSuccess -- TODO: save db
+--        otherwise -> return ()
 
 maybeRead :: Read a => String -> Maybe (a,String)
 maybeRead = listToMaybe . reads
 
-parse_rname_tspan_cname :: String -> Maybe (RegName, Timespan, CourseName)
-parse_rname_tspan_cname args = do
-    (rname,arg2) <- maybeRead args
-    (tspan,arg3) <- maybeRead arg2
-    (cname,_)    <- maybeRead arg3
-    return (rname,tspan,cname)
-
-parse_rname_cname :: String -> Maybe (RegName, CourseName)
-parse_rname_cname args = do
-    (rname,arg2) <- maybeRead args
-    (cname,_)    <- maybeRead arg2
-    return (rname,cname)
-
-parse_con_rname_cname :: String -> Maybe (Constraint, RegName, CourseName)
-parse_con_rname_cname args = do
-    (con,arg2)   <- maybeRead args
-    (rname,arg3) <- maybeRead arg2
-    (cname,_)    <- maybeRead arg3
-    return (con,rname,cname)
-
-parse_stud_rname_cname :: String -> Maybe (Student, RegName, CourseName)
-parse_stud_rname_cname args = do
-    (stud,arg2)  <- maybeRead args
-    (rname,arg3) <- maybeRead arg2
-    (cname,_)    <- maybeRead arg3
-    return (stud,rname,cname)
 
 
-pString :: String -> Parser String
-pString name = Parser $ \s -> case maybeRead s of
-                                 Nothing -> Left ("error parsing " ++ name)
-                                 Just (str,rest) -> Right (str,rest)
+data Cmd = ShowCourses 
+         | AddCourse CourseName 
+         | RemoveCourse CourseName
+         | ShowRegs CourseName 
+         | AddReg CourseName RegName Timespan
+         | RemoveReg CourseName RegName 
+         | AddCon CourseName RegName Constraint
+         | ShowStudents 
+         | ShowStudentRegs Student 
+         | RegisterStudent CourseName RegName Student
+         | Quit
+         | Debug
+         deriving (Read,Show)
 
-pCourse :: Courses -> Parser CourseName
-pCourse cs = do
-    cname <- pString "course name"
-    if Map.member cname cs
-        then return cname
-        else fail ("unknown course: " ++ cname)
+main = do let db = testdb  -- TODO: load db
+          runStateT (runErrorT loop) db
 
-newtype Parser a = Parser { parse :: String -> Either String (a,String) }
+loop :: ErrorT String (StateT Courses IO) ()
+loop = (parse >>= eval >> loop) `catchError` handler
+    where handler e = do liftIO $ putStrLn e
+                         loop
 
-instance Monad Parser where
-    return a = Parser $ \cs -> Right (a,cs)
-    fail msg = Parser $ \cs -> Left msg
-    p >>= f = Parser $ \cs -> case parse p cs of
-                                 Left msg -> Left msg
-                                 Right (a,cs') -> parse (f a) cs'
+parse :: ErrorT String (StateT Courses IO) Cmd
+parse = do liftIO $ putStr "> "
+           liftIO $ hFlush stdout
+           line <- liftIO getLine            
+           case maybeRead line of
+              Nothing -> throwError ("invalid input")
+              Just (cmd,_) -> return cmd
+
+eval :: Cmd -> ErrorT String (StateT Courses IO) ()
+eval (ShowCourses)         = do cnames <- liftM courseNames get
+                                liftIO $ mapM_ putStrLn cnames
+eval (AddCourse cname)     = do modify (addCourse cname)
+eval (RemoveCourse cname)  = do courses <- get
+                                if Map.member cname courses
+                                    then modify (removeCourse cname)
+                                    else throwError ("unknown course: " ++ cname)
+--eval (ShowRegs CourseName)
+--eval (AddReg CourseName RegName Timespan)
+--eval (RemoveReg CourseName RegName)
+--eval (AddCon CourseName RegName Constraint)
+--eval (ShowStudents)
+--eval (ShowStudentRegs Student)
+--eval (RegisterStudent CourseName RegName Student)
+eval (Quit) = do liftIO exitSuccess -- TODO: save db
+eval (Debug) = do state <- get
+                  liftIO $ print state
+
